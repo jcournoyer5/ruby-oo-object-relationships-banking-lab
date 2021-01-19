5900d07b16b8d94f9af84727674043d1057e1bc7
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-19 03:17:51 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    
+    attr_accessor :balance, :status
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end    
+    
+    def deposit
+        
+    end
+    
+    def display_blance
+        @balance
+    end
+    
+    def valid?
+        if @status == "open" && balance.self > 0
+            "valid"
+        end    
+    end   
+    
+    def close_account
+        BankAccount.clear.all
+    end    
+        
 
 end
