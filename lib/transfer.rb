1b1476e18e179518fc367a9b1acbaf3f85d5bae0
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-19 23:23:55 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,15 +12,36 @@ class Transfer
   end
   
   def valid?
+      # status == "open" && balance > 0
+      sender.valid? && receiver.valid?
       
   end      
   
   def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
       
   end     
   
   def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
   
-  end      
+  end   
+  
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end  
   
 end
